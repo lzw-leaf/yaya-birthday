5e1198b20d1f14301744cbc38af02048b272753e
lzw-leaf <1604748707@qq.com>
2023-08-01 09:21:28 +0800

    feat(global): 初始化更新

---
+++ src/components/LoveTree.vue
@@ -0,0 +1,12 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from "vue"
+
+onMounted(() => {})
+</script>
+
+<style lang="scss" scoped></style>
+s
---
+++ src/components/PoetryForYou.vue
@@ -0,0 +1,32 @@
+<template>
+  <VueWriter
+    v-for="(poetry, index) of poetryList"
+    v-show="index === currentIndex"
+    :key="poetry[0] + index"
+    :array="poetry"
+    :iterations="1"
+    @typed="onTyped"
+  >
+  </VueWriter>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from "vue"
+import VueWriter from "vue-writer"
+const currentIndex = ref(0)
+const onTyped = () => {
+  console.log("触发事件")
+}
+const poetryList = ref([
+  ["sfsf"],
+  ["sfsf"],
+  ["sfsf"],
+  ["sfsf"],
+  ["sfsf"],
+  ["sfsf"]
+])
+
+onMounted(() => {})
+</script>
+
+<style lang="scss" scoped></style>
