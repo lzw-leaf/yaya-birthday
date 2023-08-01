e6c1a5ac0c189207d7a5819e71e1af583c7e97b7
lzw-leaf <1604748707@qq.com>
2023-08-01 11:38:27 +0800

    feat(global): 丫丫生日诗歌

--- src/components/PoetryBoard.vue
+++ src/components/PoetryBoard.vue
@@ -1,17 +1,20 @@
 <template>
-  <EPTypeWriter
-    v-for="(poetry, index) of textList"
-    v-show="currentIndex >= index"
-    :key="poetry[0] + index"
-    :text="poetry"
-    @typed="currentIndex = index + 1"
-  >
-  </EPTypeWriter>
+  <div>
+    <template v-for="(poetry, index) of textList">
+      <EPTypeWriter
+        v-if="currentIndex >= index"
+        :key="poetry[0] + index"
+        :text="poetry"
+        @typed="onTyped"
+      >
+      </EPTypeWriter>
+    </template>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from "vue"
-
+import EPTypeWriter from "./EPTypeWriter.vue"
 interface PoetryBoardProps {
   textList: string[]
 }
@@ -20,9 +23,13 @@ const props = withDefaults(defineProps<PoetryBoardProps>(), {
   textList: () => [""]
 })
 const emits = defineEmits(["typed"])
-
 const currentIndex = ref(0)
-
+const onTyped = () => {
+  currentIndex.value++
+  if (currentIndex.value > props.textList.length - 1) {
+    setTimeout(() => emits("typed"), 1000)
+  }
+}
 onMounted(() => {})
 </script>
 
--- src/components/PoetryForYou.vue
+++ src/components/PoetryForYou.vue
@@ -1,19 +1,28 @@
 <template>
-  <PoetryBoard
-    v-for="(poetry, index) of poetryList"
-    v-show="index === currentIndex"
-    :key="poetry[0] + index"
-    :textList="poetry"
-    @typed="onTyped"
-  >
-  </PoetryBoard>
+  <template v-for="(poetry, index) of poetryList">
+    <Transition name="fade" mode="out-in">
+      <PoetryBoard
+        v-if="index === currentIndex && isTransition"
+        :key="index"
+        :textList="poetry"
+        @typed="onTyped"
+      >
+      </PoetryBoard>
+    </Transition>
+  </template>
 </template>
 
 <script setup lang="ts">
 import { onMounted, ref } from "vue"
 import PoetryBoard from "./PoetryBoard.vue"
 const currentIndex = ref(0)
+const isTransition = ref(true)
 const onTyped = () => {
+  isTransition.value = false
+  setTimeout(() => {
+    isTransition.value = true
+  }, 500)
+  currentIndex.value++
   console.log("触发事件")
 }
 
@@ -23,11 +32,21 @@ const poetryList = ref([
   ["你就像那丛林穿梭的溪流", "抚平我的眉间", "平息了晦涩的躁狂"],
   ["你像天边飘渺的云", "你像家中温暖的窝"],
   ["你像孩童手中的甜", "你像大人梦中的乐"],
-  ["你是我过去美好的猜想", "你是我现在手中的紧握", "你也是我未来坚定的执着"],
-  ["愿生活如愿，愿你我携手,勇往直前!", "--咩咩--"]
+  ["你是我过去美好的猜想", "你是我现在手中的紧握", "你是我未来坚定的执着"],
+  ["愿生活如愿，愿你我携手,勇往直前!"],
+  ["今天是丫丫值得纪念的日子，祝丫丫生日快乐!", "--咩咩--"]
 ])
 
 onMounted(() => {})
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
