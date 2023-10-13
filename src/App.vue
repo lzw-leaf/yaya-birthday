9a96304e4783010b15bf33178f1ddd601a7a3bc0
lzw-leaf <1604748707@qq.com>
2023-10-13 13:30:48 +0800

    feat:简单支持响应式

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
+import { onBeforeMount, provide, ref } from "vue"
 import LoveTree from "./components/LoveTree/index.vue"
+
+const widthScale = ref(1)
+const heightScale = ref(1)
+provide(widthScale, widthScale)
+provide(heightScale, heightScale)
+const updateScreenScale = () => {
+  widthScale.value = window.innerWidth / 1920
+  heightScale.value = window.innerHeight / 1080
+}
+onBeforeMount(() => {
+  updateScreenScale()
+  window.addEventListener("resize", updateScreenScale)
+})
 </script>
 
 <template>
-  <div class="app">
+  <div class="app" :style="{ scale: widthScale }">
     <LoveTree></LoveTree>
   </div>
 </template>
